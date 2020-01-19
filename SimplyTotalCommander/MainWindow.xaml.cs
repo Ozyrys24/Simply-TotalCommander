ce10b3e4673a8ba63b8826eeb5806a5a4d7047d3
Wojtaszek91 <spiochtar@o2.pl>
2020-01-19 05:46:48 +0100

    prevent windows from minimalize
    Prevent windows from minimalize and add readonly to dataGrid items

--- SimplyTotalCommander/MainWindow.xaml.cs
+++ SimplyTotalCommander/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿
+using System;
 using System.Windows;
 
 
@@ -12,11 +13,29 @@ namespace SimplyTotalCommander
         public MainWindow()
         {
             InitializeComponent();
+            SourceInitialized += new EventHandler(MainWindow_NoResizeWindowOnDoubleClick);
+        }
+        void MainWindow_NoResizeWindowOnDoubleClick(object sender, EventArgs e)
+        {
+            System.Windows.Interop.HwndSource source = System.Windows.Interop.HwndSource.FromHwnd(new System.Windows.Interop.WindowInteropHelper(this).Handle);
+            source.AddHook(new System.Windows.Interop.HwndSourceHook(WndProc));
+        }
+
+        int WM_NCLBUTTONDBLCLK { get { return 0x00A3; } }
+
+        private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            if (msg == WM_NCLBUTTONDBLCLK)
+            {
+                handled = true;  //prevent double click from resize the window.
+            }
+
+            return IntPtr.Zero;
         }
 
         private void WindowControl_Loaded(object sender, RoutedEventArgs e)
         {
-            
+
         }
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
