5d04bf54db00d4f1b7aced5ce542dd6d0a63db5c
Wojtaszek91 <spiochtar@o2.pl>
2020-01-19 01:11:02 +0100

    add PropertiesWindow
    add property window with binding object and customize as showDialog();

---
+++ SimplyTotalCommander/PropertiesWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using ClassLibrary1;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+
+namespace SimplyTotalCommander
+{
+    /// <summary>
+    /// Logika interakcji dla klasy PropertiesWindow.xaml
+    /// </summary>
+    public partial class PropertiesWindow : Window
+    {
+        public PropertiesWindow()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
